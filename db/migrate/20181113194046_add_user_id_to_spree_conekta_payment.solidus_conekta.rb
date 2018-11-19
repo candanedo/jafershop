18678d0ef85b4a0b36bc9bb7a8de86d8f4db2afd
Eduardo Candanedo <42420295+candanedo@users.noreply.github.com>
2018-11-19 11:58:28 -0600

    Delete 20181113194046_add_user_id_to_spree_conekta_payment.solidus_conekta.rb

--- db/migrate/20181113194046_add_user_id_to_spree_conekta_payment.solidus_conekta.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from solidus_conekta (originally 20140509151116)
-class AddUserIdToSpreeConektaPayment < SolidusSupport::Migration[4.2]
-  def change
-    add_column :spree_conekta_payments, :user_id, :integer
-    add_index :spree_conekta_payments, :user_id
-  end
-end
