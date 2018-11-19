f52354210aaec78e234b7bfbf4d417a0465b6d0f
Eduardo Candanedo <42420295+candanedo@users.noreply.github.com>
2018-11-19 11:58:06 -0600

    Delete 20181113194045_add_payment_method_id_to_spree_conekta_payment.solidus_conekta.rb

--- db/migrate/20181113194045_add_payment_method_id_to_spree_conekta_payment.solidus_conekta.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from solidus_conekta (originally 20140509145837)
-class AddPaymentMethodIdToSpreeConektaPayment < SolidusSupport::Migration[4.2]
-  def change
-    add_column :spree_conekta_payments, :payment_method_id, :integer
-    add_index :spree_conekta_payments, :payment_method_id
-  end
-end
